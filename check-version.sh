86b0a38776b62e7231960dd7a0682f65b8f62251
Nikita Tarasov <nikita@mygento.ru>
2021-07-15 12:05:53 +0300

    init

---
+++ check-version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+targetName=$(curl -sI "https://dl.pstmn.io/download/latest/linux64" | grep "content-disposition" | awk -F '=' '{ print $2 }')
+versionMaj=$(echo "$targetName" | awk -F '-' '{ print $4 }' | awk -F '.' '{ print $1 }')
+versionMin=$(echo "$targetName" | awk -F '-' '{ print $4 }' | awk -F '.' '{ print $2 }')
+versionRev=$(echo "$targetName" | awk -F '-' '{ print $4 }' | awk -F '.' '{ print $3 }')
+version="$versionMaj.$versionMin.$versionRev"
+latest=`cat latest`
+
+echo "Most recent Postman version $version"
+echo "Comparing with version $latest"
+if [ "$version" = "$latest" ]; then echo "EQUAL"; touch equal; else echo "NOT EQUAL";fi;
